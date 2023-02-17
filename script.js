bd552b8c4cfdb4ce31b8bb830b1ad076a62a6940
vader <jkumar8455@gmail.com>
2023-02-17 19:50:18 +0530

    second commit

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 var lastmove=Date.now();
-let songList=["songs/Kudiyee Ni Teri Selfiee 320 Kbps.mp3","songs/Main Khiladi Selfiee 320 Kbps.mp3","songs/Naiyo Lagda Kisi Ka Bhai Kisi Ki Jaan 320 Kbps.mp3","songs/Pyaar Hota Kayi Baar Hai Tu Jhoothi Main Makkaar 320 Kbps.mp3","songs/Saara Zamaana Khussh 320 Kbps.mp3","songs/tu-mera-koi-na-59338.mp3"];
+let songList=["Kudiyee Ni Teri Selfiee 320 Kbps.mp3","Main Khiladi Selfiee 320 Kbps.mp3","Naiyo Lagda Kisi Ka Bhai Kisi Ki Jaan 320 Kbps.mp3","Pyaar Hota Kayi Baar Hai Tu Jhoothi Main Makkaar 320 Kbps.mp3","Saara Zamaana Khussh 320 Kbps.mp3","tu-mera-koi-na-59338.mp3"];
 let masterPlay=document.getElementById("masterPlay");
 let prevSong=document.getElementById("prevSong");
 let nextSong=document.getElementById("nextSong");
 let gif=document.getElementById("gif");
+let currentTimer=document.getElementById("currentTimer");
+let songDuration=document.getElementById("songDuration");
 let isplaying=false;
 let cur_index=0;
 let songElement=[];
 let progressBar=document.getElementById("progressBar");
 songList.forEach((e)=>{
-    let audio=new Audio(e);
+    let audio=new Audio(`songs/${e}`);
     songElement.push(audio);
 })
 let currentSong=songElement[cur_index];
 let songName=document.getElementById("songName");
-
-function load(){
+function changeSongName(){
     songName.innerText=songList[cur_index];
+    let val=parseInt(currentSong.duration ? currentSong.duration:209);
+    let minute=parseInt(val/60);
+    let second=val%60;
+    let s="";
+    if(minute<10) s+="0";
+    s+=minute+":";
+    if(second<10) s+="0";
+    s+=second;
+    songDuration.innerText=s;
 }
-load();
+function changeCurrentTime(){
+    let val=parseInt(currentSong.currentTime);
+    let minute=parseInt(val/60);
+    let second=val%60;
+    let s="";
+    if(minute<10) s+="0";
+    s+=minute+":";
+    if(second<10) s+="0";
+    s+=second;
+    currentTimer.innerText=s;
+}
+changeSongName();
+changeCurrentTime();
 prevSong.addEventListener("click",(e)=>{
     cur_index--;
     if(cur_index<0) cur_index=songElement.length-1;
-    load();
+    changeSongName();
     isplaying=false;
     pauseSong();
     playSong();
@@ -31,7 +53,7 @@ prevSong.addEventListener("click",(e)=>{
 nextSong.addEventListener("click",(e)=>{
     cur_index++;
     if(cur_index===songElement.length) cur_index=0;
-    load();
+    changeSongName();
     isplaying=false;
     pauseSong();
     playSong();
@@ -64,17 +86,26 @@ function pauseSong(){
 
 songElement.forEach((e)=>{
     e.addEventListener("timeupdate",()=>{
-        if(Date.now()-lastmove>=1000){
+        if(currentSong.currentTime===currentSong.duration){
+            progressBar.value=100;
+            pauseSong();
+            cur_index++;
+            if(cur_index===songElement.length) cur_index=0;
+            changeSongName();
+            playSong();
+        }
+        else if(Date.now()-lastmove>=1000){
             let progress=parseInt(currentSong.currentTime/currentSong.duration*100);
-            console.log(progress);
             progressBar.value=progress;
             lastmove=Date.now();
+            changeCurrentTime();
+            changeSongName();
         }
     })
 })
 
 progressBar.addEventListener("change",(e)=>{
     let progress=e.target.value;
-    currentSong.currentTime=(progress*currentSong.duration/100);
+    currentSong.currentTime=((progress*currentSong.duration)/100);
     progressBar.value=progress;
 })
